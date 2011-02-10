2d162ac62931f07ef00b4c466bd4b0e29b1364c7
Javier Vegas <javiervegas@Javier-Vegass-MacBook-Pro.local>
2011-02-09 22:40:11 -0800

    project scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkedin::Application.config.secret_token = '94bffca7835a04d10fa89467ca2d768cb8d3cec36eaf05fb4679873b889707a3fdeabc735741f4de9b07feb5559a444a2103d150dfa5e78aa845a32dab0ef7e0'
