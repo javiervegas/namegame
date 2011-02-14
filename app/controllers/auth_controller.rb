b4a199d63f578e0a3db2569b1093d7bafd205387
Javier Vegas <javiervegas@Javier-Vegass-MacBook-Pro.local>
2011-02-13 19:30:18 -0800

    simple refactor

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,11 +4,7 @@ require 'linkedin'
 class AuthController < ApplicationController
 
   def index
-    # get your api keys at https://www.linkedin.com/secure/developer
-    client = LinkedIn::Client.new(
-        "NBxTSeJiKjsnPH6pFR9-B2lhqlDb4LZNMuzL63YuyuLKqhdwhQVzWa3kP0Exgnnk",
-        "PGg4Yl6xvkrLveG7jKq1eFbLrkWINV5AH0eY-TaYBGWOWi-XjpxuLXGFk9YW35p7"
-    )
+    client = get_client
     request_token = client.request_token(:oauth_callback =>
                                       "http://#{request.host_with_port}/auth/callback")
     session[:rtoken] = request_token.token
@@ -19,10 +15,7 @@ class AuthController < ApplicationController
   end
 
   def callback
-    client = LinkedIn::Client.new(
-        "NBxTSeJiKjsnPH6pFR9-B2lhqlDb4LZNMuzL63YuyuLKqhdwhQVzWa3kP0Exgnnk",
-        "PGg4Yl6xvkrLveG7jKq1eFbLrkWINV5AH0eY-TaYBGWOWi-XjpxuLXGFk9YW35p7"
-    )
+    client = get_client
     if session[:atoken].nil?
       pin = params[:oauth_verifier]
       atoken, asecret = client.authorize_from_request(session[:rtoken], session[:rsecret], pin)
@@ -31,20 +24,18 @@ class AuthController < ApplicationController
     else
       client.authorize_from_access(session[:atoken], session[:asecret])
     end
-    if !session[:pdate].nil?
-      client.share({
-         :comment => "Testing out the LinkedIn API",
-         :title => "Sfhare",
-         :url => "http://www.linkedin.com",
-         :image_url => "http://www.southdacola.com/blog/wp-content/uploads/2009/09/scooby-doo.jpeg"
-     })
-      client.update_status("schhmurfs!")
-    end
     @profile = client.profile
     connections = client.connections.find_all{|connection| !connection.picture_url.empty?}.sort_by{ rand }.slice(0...5)
-    @guess = connections.sort_by{ rand }.first
+    @mistery = connections.sort_by{ rand }.first
     @connections = connections.sort_by{ rand }
     #@updates = client.network_updates(:type => "SHAR").updates
+  end
+
+  def get_client
+    client = LinkedIn::Client.new(
+        "NBxTSeJiKjsnPH6pFR9-B2lhqlDb4LZNMuzL63YuyuLKqhdwhQVzWa3kP0Exgnnk",
+        "PGg4Yl6xvkrLveG7jKq1eFbLrkWINV5AH0eY-TaYBGWOWi-XjpxuLXGFk9YW35p7"
+    )
   end
 end
 
